1e43fd78b8800e906ee8e7bf2cba86fe85384459
Amit123456987 <amitmaurya11042000@gmail.com>
2022-11-27 09:34:14 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js'
+import { NavBar } from './components/NavBar';
+
+function App() {
+  return (
+    <NavBar/>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+export const NavBar = () => {
+  return (
+   
+        
+        <nav style={{'padding':'10px'}} className="navbar navbar-expand-lg navbar-light bg-dark">
+          <a className="navbar-brand" href="#">Amit Maurya</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <FontAwesomeIcon icon={faBars} style={{'color':'white'}}/>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="#">About Me</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Tech Stack</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Projects</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Teck Stack</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Teck Stack</a>
+              </li>
+            </ul>
+          
+          </div>
+        </nav>
+ 
+  );
+}
